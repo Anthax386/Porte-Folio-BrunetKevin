4b65cef23cfe07bd3d833db192abb427b59606c1
Anthax386 <kevinanthax@protonmail.com>
2023-11-15 18:38:22 +0100

    nouvelle architecture du js

---
+++ asset/scripts/Templates/technos.js
@@ -0,0 +1,30 @@
+import technoLang from'../../JSON/technoLang.json' assert{type: 'json'};
+import technoTool from'../../JSON/technoTool.json' assert{type: 'json'};
+
+export function getTechnoLang(){
+  const $sectionTechno = document.getElementById('technoLang');
+  
+  technoLang.forEach(element => {
+    const $technoElement = document.createElement("article");
+    $technoElement.classList.add("btn");
+    $sectionTechno.appendChild($technoElement);
+      $technoElement.innerHTML=`
+        <img src="${element.image}" alt="${element.imageAlt}"></img>
+        <p>${element.name}</p>
+      `;
+  });
+};
+
+export function getTechnoTool(){
+  const $sectionTechno = document.getElementById('technoTool');
+  
+  technoTool.forEach(element => {
+    const $technoElement = document.createElement("article");
+    $technoElement.classList.add("btn");
+    $sectionTechno.appendChild($technoElement);
+      $technoElement.innerHTML=`
+        <img src="${element.image}" alt="${element.imageAlt}"></img>
+        <p>${element.name}</p>
+      `;
+  });
+};
---
+++ asset/scripts/scripts.js
@@ -0,0 +1,7 @@
+import { getWorks } from "./Templates/work.js";
+import { getTechnoLang, getTechnoTool } from "./Templates/technos.js"
+
+getWorks();
+
+getTechnoLang();
+getTechnoTool();
