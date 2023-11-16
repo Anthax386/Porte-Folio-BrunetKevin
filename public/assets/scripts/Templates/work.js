594c1667f88c36378a160236a064719a0f784bd6
Anthax386 <kevinanthax@protonmail.com>
2023-11-16 17:53:24 +0100

    change title class

--- public/assets/scripts/Templates/work.js
+++ public/assets/scripts/Templates/work.js
@@ -4,7 +4,7 @@ export function getWorks (){
     /*Selection de la section works*/
     const $sectionWorks = document.querySelector('.works');
         $sectionWorks.innerHTML = `
-            <h2 class="section-title" id="projets">Mes projets</h2>
+            <h2 class="title" id="projets">Mes projets</h2>
             <div class="projets"></div>
         `;
     works.forEach(element => {
@@ -13,7 +13,7 @@ export function getWorks (){
             $workArticle.classList.add('projet');
             $workArticle.innerHTML= `
                 <div class="projet_readme">
-                    <h3>${element.name}</h3>
+                    <h3 class="title">${element.name}</h3>
                     <p id="readme">${element.readme_short}</p>
                 </div>
                 <div class="projet_preview">
